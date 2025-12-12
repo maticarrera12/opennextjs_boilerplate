88cd915e9dfbd194065a68718cbb786e78409e6b
MatiCarrera <mcarreradev12@gmail.com>
2025-12-12 17:25:52 -0300

    new bg

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,11 @@
+import type { NextRequest } from "next/server";
 import createMiddleware from "next-intl/middleware";
 
 import { routing } from "./src/i18n/routing";
 
 const intlMiddleware = createMiddleware(routing);
 
-export function proxy(request: Request) {
+export function proxy(request: NextRequest) {
   return intlMiddleware(request);
 }
 
